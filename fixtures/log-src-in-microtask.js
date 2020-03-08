87ccc1a30987b6b71605bf56c3b3bcabc8be85b4
Haoqun Jiang <haoqunjiang@gmail.com>
2020-03-08 13:13:29 +0800

    test: simplify IE test

--- fixtures/log-src-in-microtask.js
+++ fixtures/log-src-in-microtask.js
@@ -1,7 +1,7 @@
 var app = document.getElementById('app')
 if (typeof Promise === 'undefined') {
   // skip this test in IE
-  app.textContent = 'http://localhost:5000/fixtures/log-src-in-microtask.js'
+  app.textContent = window.getCurrentScript().src
 } else {
   Promise.resolve().then(function() {
     app.textContent = window.getCurrentScript().src
